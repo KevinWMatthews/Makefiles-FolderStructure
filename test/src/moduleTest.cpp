5cedab1a5704ed6ce0acb709c257fc281cc36847
Kevin Matthews <KevinNotMatt@gmail.com>
2015-03-11 07:56:33 -0700

    Add basic test to module

--- test/src/moduleTest.cpp
+++ test/src/moduleTest.cpp
@@ -1,10 +1,12 @@
 extern "C"
 {
   // #includes with C linkage
+  #include "module.h"
 }
 
 // #include with C++ linkage
 #include "CppUTest/TestHarness.h"
+#include "moduleTest.h"
 
 TEST_GROUP(moduleTest)
 {
@@ -16,7 +18,7 @@ TEST_GROUP(moduleTest)
   }
 };
 
-TEST(moduleTest, Wiring)
+TEST(moduleTest, DummyFunc)
 {
-  FAIL("moduleTest wired properly!");
+  LONGS_EQUAL(7, module_DummyFunc(7));
 }
